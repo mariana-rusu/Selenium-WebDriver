0c8a4765f570cc530cfc84dea0082390fa6d832b
mariana-rusu <mirabelia@yahoo.com>
2017-01-04 15:54:42 -0500

    Test_MultipleFiltersApplied.py  file updated

--- GuestUser/SearchJobs/Test_MultipleFiltersApplied.py
+++ GuestUser/SearchJobs/Test_MultipleFiltersApplied.py
@@ -17,22 +17,17 @@ class SearchJobsByKeywordAndByLocation(unittest.TestCase):
 
         driver = self.driver
         element1 = driver.find_element_by_id('job_locationautocomplete')
-        time.sleep(3)
         element1.send_keys("New York City Metro Area")
-        time.sleep(3)
-        element11 = driver.find_element_by_xpath("//div/section[1]/div/div/div/div/div[1]/div/form/div/div/ul/li[1]/button")
-        element11.click()
-        element2 = driver.find_element_by_xpath("//div/section[1]/div/div/div/div/div[2]/div/form/div/input")
-        element2.send_keys("QA")
+        element2 = driver.find_element_by_xpath("//div/section[1]/div/div/div/div/div[1]/div/form/div/div/ul/li[1]/button")
+        element2.click()
+        element3 = driver.find_element_by_xpath("//div/section[1]/div/div/div/div/div[2]/div/form/div/input")
+        element3.send_keys("QA")
         button = driver.find_element_by_xpath("//div/section[1]/div/div/div/div/a[1]")
         button.click()
-        time.sleep(10)
-
         applied_filter1 = driver.find_element_by_xpath("//div/div[2]/div/div[2]/div/div/ul/li[2]/button/span[1]")
         self.assertEqual(applied_filter1.text, "New York City Metro Area")
         applied_filter2 = driver.find_element_by_xpath("//div/div[2]/div/div[2]/div/div/ul/li[1]/button/span[1]")
         self.assertEqual(applied_filter2.text,"QA")
-
         headline = driver.find_element_by_xpath("//*[@id='listing']/div[1]/h1")
         self.assertEqual(headline.text, "QA JOBS IN NEW YORK CITY METRO AREA")
 
